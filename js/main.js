cbc4fa875c9e7a1259153000e13048a558c29999
Muhammad Akbar <ramazonov3600@gmail.com>
2022-11-28 21:49:26 +0500

    add modal

--- js/main.js
+++ js/main.js
@@ -8,7 +8,22 @@ const elFormIput = document.querySelector(".site-search-input");
 
 // modal
 // Get the modal
+const renderDiv = document.querySelector(".render-modal");
 const modal = document.getElementById("myModal");
+const elModalTemp = document.querySelector(".modal-temp").content;
+
+
+
+
+var example = {
+    foo1: { /* stuff1 */},
+    foo2: { /* stuff2 */},
+    foo3: { /* stuff3 */}
+};
+
+var keys = Object.keys(example)[0];
+
+console.log(keys);
 
 // Get the button that opens the modal
 const btn = document.getElementById("myBtn");
@@ -21,6 +36,25 @@ elBtn.addEventListener('click', () => {
     document.body.classList.toggle('theme-dars')
 });
 
+function renderModal(arr, node){
+    node.innerHTML = "";
+    console.log(arr);
+    arr.forEach(element => {
+        
+        const elModalCloneTemp = elModalTemp.cloneNode(true);
+        elModalCloneTemp.querySelector(".modal-img").src = element.flags.svg;
+        elModalCloneTemp.querySelector(".modal-title").textContent = element.name.common;
+        elModalCloneTemp.querySelector(".modal-region").textContent = element.region;
+        elModalCloneTemp.querySelector(".modal-currencies").textContent = [Object.keys(element.currencies)[0]];
+        elModalCloneTemp.querySelector(".modal-language").textContent = [Object.values(element.languages)[0]];
+        elModalCloneTemp.querySelector(".modal-subregion").textContent = element.subregion;
+        elModalCloneTemp.querySelector(".modal-link").href = element.maps.googleMaps;
+        
+        node.appendChild(elModalCloneTemp);
+    })
+    
+}
+
 // render arr
 function render(arr, node){
     node.innerHTML = "";
@@ -32,18 +66,21 @@ function render(arr, node){
         elCloneTemp.querySelector(".population-desc").textContent = element.population;
         elCloneTemp.querySelector(".region-desc").textContent = element.region;
         elCloneTemp.querySelector(".capital-desc").textContent = element.capital;
+        elCloneTemp.querySelector(".more-info-btn").dataset.id = element.name.common;
+        
         node.appendChild(elCloneTemp);
-
+        
     });
 };
 
+
 // get arr
-async function getRegion(url) {
+async function getRegion(url, node = elList, getRender = render) {
     const res = await fetch(url);
     const data = await res.json();
-    
-    render(data, elList);
+    getRender(data, node);
 };
+
 
 getRegion("https://restcountries.com/v3.1/all");
 
@@ -61,10 +98,13 @@ elForm.addEventListener("submit", (evt)=> {
     
 });
 
+elList.addEventListener("click", evt=>{
+    if (evt.target.matches(".more-info-btn")) {
+        getRegion(`https://restcountries.com/v3.1/name/${evt.target.dataset.id}`, renderDiv, renderModal);
+    }
+});
+
 // modal 
-
-
-
 // When the user clicks on the button, open the modal
 elList.addEventListener("click", evt =>{
     evt.preventDefault();
@@ -72,7 +112,7 @@ elList.addEventListener("click", evt =>{
         document.body.style.overflow = "hidden"
         modal.style.display = "block";  
         modal.style.overflow = "hidden";        
-
+        
     }
 });
 
@@ -80,8 +120,6 @@ elList.addEventListener("click", evt =>{
 span.onclick = function() {
     modal.style.display = "none";
     document.body.style.overflow = "scroll"
-
-
 };
 
 // When the user clicks anywhere outside of the modal, close it
