752001597f9e79d60bc1102ac5198cd990c4acaa
Muhammad Akbar <ramazonov3600@gmail.com>
2022-11-28 11:34:22 +0500

    bag fix

--- js/main.js
+++ js/main.js
@@ -6,6 +6,16 @@ const elSelect = document.querySelector(".form-selects");
 const elForm = document.querySelector(".site-search");
 const elFormIput = document.querySelector(".site-search-input");
 
+// modal
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
 // darkMode
 elBtn.addEventListener('click', () => {
     document.body.classList.toggle('theme-dars')
@@ -23,6 +33,7 @@ function render(arr, node){
         elCloneTemp.querySelector(".region-desc").textContent = element.region;
         elCloneTemp.querySelector(".capital-desc").textContent = element.capital;
         node.appendChild(elCloneTemp);
+
     });
 };
 
@@ -44,8 +55,39 @@ elSelect.addEventListener("change", ()=> {
 
 elForm.addEventListener("submit", (evt)=> {
     evt.preventDefault();
-
+    
     const elFormIputValue = elFormIput.value; 
     getRegion(`https://restcountries.com/v3.1/name/${elFormIputValue}`);
-
+    
 });
+
+// modal 
+
+
+
+// When the user clicks on the button, open the modal
+elList.addEventListener("click", evt =>{
+    evt.preventDefault();
+    if (evt.target.matches("#myBtn")) {
+        document.body.style.overflow = "hidden"
+        modal.style.display = "block";  
+        modal.style.overflow = "hidden";        
+
+    }
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+    document.body.style.overflow = "scroll"
+
+
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.body.style.overflow = "scroll"
+    }
+};
